3162dbe4a4a8f505a5c68ff8fa8d03fe1ad867b2
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-01-18 14:46:30 +0530

    Test: Add tests for InventoryItemId

---
+++ tests/eShop.Domain.Tests/Inventory/InventoryItemIdTests.cs
@@ -0,0 +1,36 @@
+using eShop.Domain.Inventory;
+
+namespace eShop.Domain.Tests.Inventory;
+
+public class InventoryItemIdTests
+{
+    [Fact]
+    public void InventoryItemId_Create_WithValidGuid_ReturnsInventoryItemIdObject()
+    {
+        var guid = Guid.NewGuid();
+        var inventoryItemId = new InventoryItemId(guid);
+        Assert.Equal(guid, inventoryItemId.Value);
+    }
+
+    [Fact]
+    public void InventoryItemId_Equality_WithSameValue_ReturnsTrue()
+    {
+        var guid = Guid.NewGuid();
+        var id1 = new InventoryItemId(guid);
+        var id2 = new InventoryItemId(guid);
+
+        Assert.True(id1.Equals(id2));
+        Assert.True(id1 == id2);
+        Assert.Equal(id1.GetHashCode(), id2.GetHashCode());
+    }
+
+    [Fact]
+    public void InventoryItemId_Inequality_WithDifferentValue_ReturnsFalse()
+    {
+        var id1 = new InventoryItemId(Guid.NewGuid());
+        var id2 = new InventoryItemId(Guid.NewGuid());
+
+        Assert.False(id1.Equals(id2));
+        Assert.True(id1 != id2);
+    }
+}
