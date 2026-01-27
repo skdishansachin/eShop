2be5171809f4bc4ca49ad300fb5f90ea90724943
skdishansachin <134765302+skdishansachin@users.noreply.github.com>
2026-01-27 13:54:49 +0530

    feat(infrastructure): Add DependencyInjection for Infrastructure services

---
+++ src/eShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+using eShop.Infrastructure.Presistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eShop.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+        );
+
+        return services;
+    }
+}
